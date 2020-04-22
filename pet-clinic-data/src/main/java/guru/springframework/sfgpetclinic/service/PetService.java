b9f397f71a666fffb71e638afa1ba448becf4377
yadavShubham07 <shubhamy2628@gmail.com>
2020-04-22 16:50:10 +0530

    Creating Interfaces. closes #5

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/PetService.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.service;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+
+}
+
