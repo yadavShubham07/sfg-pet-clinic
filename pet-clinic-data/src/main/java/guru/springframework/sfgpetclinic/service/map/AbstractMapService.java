516899133ac36626eece860a4533ee9a8050b290
yadavShubham07 <shubhamy2628@gmail.com>
2020-04-23 18:15:19 +0530

    closes #6

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/AbstractMapService.java
@@ -0,0 +1,28 @@
+package guru.springframework.sfgpetclinic.service.map;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public abstract class AbstractMapService<T,ID> {
+
+    protected Map<ID,T> map = new HashMap<>();
+    Set<T> findAll(){
+        return new HashSet<>(map.values());
+    }
+    T findById(ID id){
+        return map.get(id);
+    }
+    T save(T object,ID id){
+        map.put(id,object);
+        return object;
+    }
+    void deleteById(ID id){
+        map.remove(id);
+    }
+    void delete(T object){
+        map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+}
